606d334c57c0e2d63c7fba58d726df014d6922ed
Ranjeet Shah <ranjeet.shah@mediascienceinternational.com>
2015-02-12 12:06:25 -0800

    [admin] Initial commit - Added java src and maven pom

---
+++ src/main/java/com/msiops/premailer/App.java
@@ -0,0 +1,13 @@
+package com.msiops.premailer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
